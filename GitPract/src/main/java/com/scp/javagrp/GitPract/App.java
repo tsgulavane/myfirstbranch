19a454a9be198f7509a88737d87e816b72df9aec
tsgulavane <tsgulvane@gmail.com>
2019-12-12 18:42:55 +0530

    my second try

--- GitPract/src/main/java/com/scp/javagrp/GitPract/App.java
+++ GitPract/src/main/java/com/scp/javagrp/GitPract/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	System.out.println("try");
         System.out.println( "Hello World!" );
         System.out.println("this is my first programm uploading on git");
     }
