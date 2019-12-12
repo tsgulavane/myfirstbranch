c3f90b72bf66e6d44da3874f4c7e3f15c576f457
tsgulavane <tsgulvane@gmail.com>
2019-12-12 18:34:20 +0530

    my first commit

---
+++ GitPract/src/main/java/com/scp/javagrp/GitPract/App.java
@@ -0,0 +1,14 @@
+package com.scp.javagrp.GitPract;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("this is my first programm uploading on git");
+    }
+}
