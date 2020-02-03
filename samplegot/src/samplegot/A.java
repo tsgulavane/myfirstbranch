1794cd80c674230bb81fb8a3718e1ffdf81c80dd
tsgulavane <54681977+tsgulavane@users.noreply.github.com>
2020-02-03 21:36:55 +0530

    Update A.java

--- samplegot/src/samplegot/A.java
+++ samplegot/src/samplegot/A.java
@@ -3,6 +3,7 @@ package samplegot;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("A");
+		System.out.println("A");
 		System.out.println("B");
 		System.out.println("A");
 	
