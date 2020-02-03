4586089bdea4b84aa3874c669bca50391586d395
tsgulavane <tsgulvane@gmail.com>
2020-02-03 21:21:05 +0530

    fifyth

--- samplegot/src/samplegot/A.java
+++ samplegot/src/samplegot/A.java
@@ -3,17 +3,7 @@ package samplegot;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
+	
 	}
 
 }
